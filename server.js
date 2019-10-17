0a2f352126b079c711d21a64123d4417780313c4
Paul Gain <paul.gain@digital.trade.gov.uk>
2019-10-17 21:07:37 +0100

    Add Github secrets/env vars

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Github Actions'))
+app.get('/', (req, res) => res.send(`HELLO ${process.env.FIRST_NAME} ${process.env.LAST_NAME}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
