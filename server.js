0b905f7966a069cbc611571c54a8742d34a3583c
Paul Gain <paul.gain@digital.trade.gov.uk>
2019-10-17 21:16:43 +0100

    Remove console log env vars

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-console.log(`FIRST_NAME: ${process.env.FIRST_NAME}`)
-console.log(`LAST_NAME: ${process.env.LAST_NAME}`)
-
 app.get('/', (req, res) => res.send(`HELLO ${process.env.FIRST_NAME} ${process.env.LAST_NAME}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
